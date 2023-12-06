50aae8bee3355d9f3361555f94d66ac9dc2a4d33
Tony <antony.branquinho@gmail.com>
2023-12-06 09:54:48 -0300

    Legal

---
+++ Soma vetor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma_vetor {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n, i;
+            double soma, media;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.Write("VALORES = ");
+            for (i = 0; i < n; i++) {
+                Console.Write(vet[i].ToString("F2", CI) + " ");
+            }
+
+            Console.WriteLine();
+            soma = 0;
+            for (i = 0; i < n; i++) {
+                soma = soma + vet[i];
+            }
+
+            media = soma / n;
+            Console.WriteLine("SOMA = " + soma.ToString("F2", CI));
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+        }
+    }
+}
